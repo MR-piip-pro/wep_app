dfbef214f20ba308b8dca3a68d08c8e5878717fc
MR-piip-pro <kilhantar@proton.me>
2025-06-04 23:04:14 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-// Firebase Configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyB3bBsR5j6CHYaXkrIIF_g4ZvWGYd3WTiQ",
   authDomain: "mr-piip-pro.firebaseapp.com",
   projectId: "mr-piip-pro",
-  storageBucket: "mr-piip-pro.firebasestorage.app",
+  storageBucket: "mr-piip-pro.appspot.com",
   messagingSenderId: "826519029119",
   appId: "1:826519029119:web:548c3298af3b20f370464a",
   measurementId: "G-MQ3QVBXJS4"
 };
+
